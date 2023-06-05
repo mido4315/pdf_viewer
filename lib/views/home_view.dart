6ebc401e4d5eacad0dce8890904be286c769d3b0
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-05 18:53:58 +0300

    handle home view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,12 +1,45 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class HomeView extends StatelessWidget {
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:pdf_viewer/views/pdf_show_view.dart';
+import 'package:provider/provider.dart';
+
+import '../business_logic.dart';
+import '../main.dart';
+
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
+  State<HomeView> createState() => _HomeViewState();
+}
 
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Consumer<PdfProvider>(
+        builder: (context, state, child) {
+          return Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                await state.loadFile();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PdfShowView(
+                        pdfFile: state.file,
+                      ),
+                    ));
+              },
+              child: const Text(
+                "Open",
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
