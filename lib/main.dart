e3718c737f9976b3427dd42a69f3bf1f99b9e6b0
mido4315 <mohmed.mahmoud3@gmail.com>
2023-06-05 18:54:15 +0300

    handle open with

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:pdf_viewer/business_logic.dart';
+import 'package:pdf_viewer/views/pdf_show_view.dart';
+import 'package:provider/provider.dart';
 
 import 'views/home_view.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main(List<String> args) {
+
+  final List<String> myArgs = [
+    'string 01',
+    'string 02',
+    'string 03',
+  ];
+  //args = myArgs;
+  if (args.isNotEmpty) {
+    PdfProvider.globalArgs = args;
+  }
+
+  //print("${globalArgs[0]} a777777777777a");
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => PdfProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +40,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeView(),
+      home: PdfProvider.globalArgs[0] == ''
+          ? const HomeView()
+          : PdfShowView(
+              pdfFile: File(
+                PdfProvider.globalArgs[0],
+              ),
+            ),
     );
   }
 }
